91404a8f1f438b8a3ea21f10ac386b59ede44de1
Cedric Bardaine <cedricbardaine@gmail.com>
2019-10-01 19:07:28 +0200

    add all files

---
+++ todos.php
@@ -0,0 +1,22 @@
+[
+    {
+        nomTache: "Laver la voiture",
+        contexteTache: "Maison",
+        dateTache: "03/02/19",
+        heureTache:"22h00",
+        dureeTache: "240",
+        prioritaire: true ,
+        lienTache : "",
+        completed : false
+    },
+    {
+        nomTache: "Réparer le vélo",
+        contexteTache: "Jardin",
+        dateTache: "05/02/19",
+        heureTache:"06h00",
+        dureeTache: "20",
+        prioritaire: false ,
+        lienTache : "reparetonvelo.com",
+        completed : true
+    }
+]
